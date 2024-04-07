a43512623f9d7395b476fa50cfffdbea61dc110a
Wendi Xiao <85204575+WoodyXiao@users.noreply.github.com>
2024-04-07 16:10:26 -0700

    move some methods to utils.jsx

--- src/components/GenerateSchedule.jsx
+++ src/components/GenerateSchedule.jsx
@@ -3,91 +3,9 @@ This is the button that handle the event of generating the optimized course sche
 */
 
 import React from "react";
+import {restructureSelectedCourses, simplifySelectedCourses} from "../utils/utils.jsx";
 
 const GenerateSchedule = ({ selectedCourses }) => {
-  // Function to restructure selectedCourses
-  const restructureSelectedCourses = () => {
-    const newSelectedCourses = selectedCourses.map((courseObj) => {
-      const newCourseObj = {};
-      for (const key in courseObj) {
-        if (!courseObj.hasOwnProperty(key)) continue;
-
-        const course = courseObj[key];
-        newCourseObj[key] = {
-          course: course.course,
-          lecture: course.lecture
-            ? {
-                text: course.lecture.text,
-                value: course.lecture.value,
-                sectionCode: course.lecture.sectionCode,
-                courseSchedule: course.lecture.details?.courseSchedule ?? [],
-              }
-            : null,
-          labs:
-            course.labs?.map((lab) => ({
-              text: lab.text,
-              value: lab.value,
-              sectionCode: lab.sectionCode,
-              courseSchedule: lab.details?.courseSchedule ?? [],
-            })) ?? [],
-        };
-        newCourseObj.priority = courseObj.priority;
-      }
-
-      return newCourseObj;
-    });
-
-    console.log("Restructured Selected Courses:", newSelectedCourses);
-
-    return newSelectedCourses;
-  };
-
-  // Function to simplify the selectedCourses structure
-  const simplifySelectedCourses = (selectedCourses) => {
-    return selectedCourses.map((courseObj) => {
-      const courseEntries = Object.entries(courseObj).filter(
-        ([key]) => key !== "priority"
-      );
-      const priority = courseObj.priority;
-
-      const simplifiedCourse = courseEntries.map(([key, course]) => {
-        const lecture = course.lecture
-          ? {
-              text: course.lecture.text,
-              value: course.lecture.value,
-              scheduleEntries: course.lecture.courseSchedule.map(
-                (schedule) => ({
-                  ...schedule,
-                  type: "lecture", // Adding type to distinguish between lecture and lab
-                })
-              ),
-            }
-          : null;
-
-        const labs =
-          course.labs?.map((lab) => ({
-            text: lab.text,
-            value: lab.value,
-            scheduleEntries: lab.courseSchedule.map((schedule) => ({
-              ...schedule,
-              type: "lab", // Adding type for labs
-            })),
-          })) ?? [];
-
-        return {
-          courseId: key,
-          courseName: course.course,
-          lecture,
-          labs,
-        };
-      });
-
-      return {
-        courses: simplifiedCourse,
-        priority,
-      };
-    });
-  };
 
   // ---------------- Weighted Interval Scheduling Algorithm Implementation ---------------------
 
@@ -184,11 +102,11 @@ const GenerateSchedule = ({ selectedCourses }) => {
     <div>
       <button
         onClick={() => {
-          const selectedCourses = simplifySelectedCourses(
-            restructureSelectedCourses()
+          const finalSelectedCourses = simplifySelectedCourses(
+            restructureSelectedCourses(selectedCourses)
           );
-          console.log("Selected Courses:", selectedCourses);
-          const selectedSchedule = scheduleCourses(selectedCourses);
+          console.log("Selected Courses:", finalSelectedCourses);
+          const selectedSchedule = scheduleCourses(finalSelectedCourses);
           console.log("selected schedule:", selectedSchedule);
         }}
       >
--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -31,4 +31,86 @@ const extractCourseName = (input) => {
     : "";
 };
 
-export { extractCourseNum, extractCourseName };
+// Function to restructure selectedCourse.
+const restructureSelectedCourses = (selectedCourses) => {
+  const newSelectedCourses = selectedCourses.map((courseObj) => {
+    const newCourseObj = {};
+    for (const key in courseObj) {
+      if (!courseObj.hasOwnProperty(key)) continue;
+
+      const course = courseObj[key];
+      newCourseObj[key] = {
+        course: course.course,
+        lecture: course.lecture
+          ? {
+              text: course.lecture.text,
+              value: course.lecture.value,
+              sectionCode: course.lecture.sectionCode,
+              courseSchedule: course.lecture.details?.courseSchedule ?? [],
+            }
+          : null,
+        labs:
+          course.labs?.map((lab) => ({
+            text: lab.text,
+            value: lab.value,
+            sectionCode: lab.sectionCode,
+            courseSchedule: lab.details?.courseSchedule ?? [],
+          })) ?? [],
+      };
+      newCourseObj.priority = courseObj.priority;
+    }
+
+    return newCourseObj;
+  });
+
+  console.log("Restructured Selected Courses:", newSelectedCourses);
+
+  return newSelectedCourses;
+};
+
+// Function to simplify the selectedCourses structure.
+const simplifySelectedCourses = (selectedCourses) => {
+  return selectedCourses.map((courseObj) => {
+    const courseEntries = Object.entries(courseObj).filter(
+      ([key]) => key !== "priority"
+    );
+    const priority = courseObj.priority;
+
+    const simplifiedCourse = courseEntries.map(([key, course]) => {
+      const lecture = course.lecture
+        ? {
+            text: course.lecture.text,
+            value: course.lecture.value,
+            scheduleEntries: course.lecture.courseSchedule.map((schedule) => ({
+              ...schedule,
+              type: "lecture", // Adding type to distinguish between lecture and lab
+            })),
+          }
+        : null;
+
+      const labs =
+        course.labs?.map((lab) => ({
+          text: lab.text,
+          value: lab.value,
+          scheduleEntries: lab.courseSchedule.map((schedule) => ({
+            ...schedule,
+            type: "lab", // Adding type for labs
+          })),
+        })) ?? [];
+
+      return {
+        courseId: key,
+        courseName: course.course,
+        lecture,
+        labs,
+      };
+    });
+
+    return {
+      courses: simplifiedCourse,
+      priority,
+    };
+  });
+};
+
+export { extractCourseNum, extractCourseName, restructureSelectedCourses, simplifySelectedCourses };
