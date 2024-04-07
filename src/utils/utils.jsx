830a143b52bd3ee9c9f2bf42bf38ed0d0f290417
Wendi Xiao <85204575+WoodyXiao@users.noreply.github.com>
2024-04-07 15:38:52 -0700

    fixed thw case when course is writing course

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -1,10 +1,26 @@
 // Function to extract course number from a string
 const extractCourseNum = (input) => {
-  return input.match(/\d+/g) ? input.match(/\d+/g).join("") : "";
+  // Handle the case when the input course is writing course.
+  if (input[input.length - 1] === "w") {
+    return input.match(/\d+/g) ? input.match(/\d+/g).join("") + "w" : "";
+  }
 };
 
 // Function to extract course name from a string
 const extractCourseName = (input) => {
+  // Handle the case when input course is writing course.
+  if (input[input.length - 1] === "w") {
+    if (input.match(/[^\d]+/g)) {
+      input = input
+        .match(/[^\d]+/g)
+        .join("")
+        .trim();
+
+      return input.substring(0, input.length - 1).trim();
+    } else {
+      return "";
+    }
+  }
   return input.match(/[^\d]+/g)
     ? input
         .match(/[^\d]+/g)
