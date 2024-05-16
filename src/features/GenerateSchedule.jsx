7fdefce13de2654015d1eac71ee389852b805039
Wendi Xiao <85204575+WoodyXiao@users.noreply.github.com>
2024-05-16 15:40:07 -0700

    add clear button and display schedule

--- src/features/GenerateSchedule.jsx
+++ src/features/GenerateSchedule.jsx
@@ -2,35 +2,43 @@
 This is the button that handle the event of generating the optimized course schedule by the weighted interval schedule algorithm
 */
 
-import React from "react";
-import {restructureSelectedCourses, simplifySelectedCourses} from "../utils/utils.jsx";
+import React, { useState } from "react";
+import {
+  restructureSelectedCourses,
+  simplifySelectedCourses,
+} from "../utils/utils.jsx";
+import Schedule from "./Schedule.jsx";
 
 const GenerateSchedule = ({ selectedCourses }) => {
+  const [selectedSchedule, setSelectedSchedule] = useState({
+    schedule: [],
+    totalPriority: 0,
+  });
 
   // ---------------- Weighted Interval Scheduling Algorithm Implementation ---------------------
 
-      // Function to check if two sessions overlap
-      const doSessionsOverlap = (session1, session2) => {
-        const days1 = session1.days.split(", ");
-        const days2 = session2.days.split(", ");
-        const timeOverlap = !(
-          Date.parse(`01/01/2000 ${session1.endTime}`) <=
-            Date.parse(`01/01/2000 ${session2.startTime}`) ||
-          Date.parse(`01/01/2000 ${session2.endTime}`) <=
-            Date.parse(`01/01/2000 ${session1.startTime}`)
-        );
-        return days1.some((day) => days2.includes(day)) && timeOverlap;
-      };
-  
-      // Simplefies the handling of multiple days for a single lecture or lab session
-      const expandScheduleEntries = (scheduleEntries) => {
-        return scheduleEntries.flatMap((entry) => {
-          return entry.days.split(", ").map((day) => ({
-            ...entry,
-            day, // Override the days field with the single day for comparision
-          }));
-        });
-      };
+  // Function to check if two sessions overlap
+  const doSessionsOverlap = (session1, session2) => {
+    const days1 = session1.days.split(", ");
+    const days2 = session2.days.split(", ");
+    const timeOverlap = !(
+      Date.parse(`01/01/2000 ${session1.endTime}`) <=
+        Date.parse(`01/01/2000 ${session2.startTime}`) ||
+      Date.parse(`01/01/2000 ${session2.endTime}`) <=
+        Date.parse(`01/01/2000 ${session1.startTime}`)
+    );
+    return days1.some((day) => days2.includes(day)) && timeOverlap;
+  };
+
+  // Simplefies the handling of multiple days for a single lecture or lab session
+  const expandScheduleEntries = (scheduleEntries) => {
+    return scheduleEntries.flatMap((entry) => {
+      return entry.days.split(", ").map((day) => ({
+        ...entry,
+        day, // Override the days field with the single day for comparision
+      }));
+    });
+  };
 
   // Main Function for Course Scheduling
   const scheduleCourses = (simplifiedCourses) => {
@@ -39,7 +47,7 @@ const GenerateSchedule = ({ selectedCourses }) => {
 
     const buildSchedule = (currentIndex, currentSchedule, currentPriority) => {
       if (currentIndex === simplifiedCourses.length) {
-        if(currentPriority > maxPriority) {
+        if (currentPriority > maxPriority) {
           bestSchedule = currentSchedule;
           maxPriority = currentPriority;
         }
@@ -47,28 +55,30 @@ const GenerateSchedule = ({ selectedCourses }) => {
       }
 
       const courseGroup = simplifiedCourses[currentIndex];
-      const groupPriority = parseInt(courseGroup.priority, 10) // Convert priority to integer.
+      const groupPriority = parseInt(courseGroup.priority, 10); // Convert priority to integer.
 
       courseGroup.courses.forEach((course) => {
         const lectureEntries = expandScheduleEntries(
-          course.lecture.scheduleEntries,
+          course.lecture.scheduleEntries
         );
         const labOptions = [null, ...course.labs];
 
         labOptions.forEach((lab) => {
-          const labEntries = lab ? expandScheduleEntries(lab.scheduleEntries) : [];
+          const labEntries = lab
+            ? expandScheduleEntries(lab.scheduleEntries)
+            : [];
           const allEntries = [...lectureEntries, ...labEntries];
 
-          const hasAnyConflict = allEntries.some((entry) => 
-          currentSchedule.some((scheduleCourse) =>
-            scheduleCourse.scheduleEntries.some((scheduledSession) => 
-              doSessionsOverlap(entry, scheduledSession),
-              ), 
-            ),
+          const hasAnyConflict = allEntries.some((entry) =>
+            currentSchedule.some((scheduleCourse) =>
+              scheduleCourse.scheduleEntries.some((scheduledSession) =>
+                doSessionsOverlap(entry, scheduledSession)
+              )
+            )
           );
 
           if (!hasAnyConflict) {
-            const newPriority = currentPriority + groupPriority; 
+            const newPriority = currentPriority + groupPriority;
             const newSchedule = [
               ...currentSchedule,
               {
@@ -77,8 +87,8 @@ const GenerateSchedule = ({ selectedCourses }) => {
                 lecture: course.lecture.text,
                 lab: lab ? lab.text : "",
                 scheduleEntries: allEntries,
-                priority: groupPriority, 
-              }
+                priority: groupPriority,
+              },
             ];
             buildSchedule(currentIndex + 1, newSchedule, newPriority);
           }
@@ -92,7 +102,7 @@ const GenerateSchedule = ({ selectedCourses }) => {
     // Initialize recursive scheduling an empty schedule and zero total priority.
     buildSchedule(0, [], 0);
 
-    return {schedule: bestSchedule, totalMaxPriority: maxPriority};
+    return { schedule: bestSchedule, totalMaxPriority: maxPriority };
   };
 
   return (
@@ -100,15 +110,32 @@ const GenerateSchedule = ({ selectedCourses }) => {
       <button
         onClick={() => {
           const finalSelectedCourses = simplifySelectedCourses(
-            restructureSelectedCourses(selectedCourses)
+            restructureSelectedCourses(selectedCourses),
           );
           console.log("Selected Courses:", finalSelectedCourses);
-          const selectedSchedule = scheduleCourses(finalSelectedCourses);
-          console.log("selected schedule:", selectedSchedule);
+          const newSelectedSchedule = scheduleCourses(finalSelectedCourses);
+          console.log("Selected Schedule:", newSelectedSchedule);
+          setSelectedSchedule(newSelectedSchedule);
         }}
       >
-        Restructure Selected Courses
+        Generate Schedule
       </button>
+      <button
+        onClick={() =>
+          setSelectedSchedule({
+            schedule: [],
+            totalPriority: 0,
+          })
+        }
+      >
+        clear
+      </button>
+      {selectedSchedule.schedule.length !== 0 && (
+        <Schedule
+          schedule={selectedSchedule.schedule}
+          totalPriority={selectedSchedule.totalPriority}
+        />
+      )}
     </div>
   );
 };
